fb6796415e85669499d0f8c7ca065d4c8d3e7bba
Simon Bierozko <105741786+simon1494@users.noreply.github.com>
2024-08-21 20:54:48 -0300

    2024-08-21

--- Imperativo/ProgramaNumAleatorio.pas
+++ Imperativo/ProgramaNumAleatorio.pas
@@ -1,6 +1,27 @@
 program ProgramaNumAleatorio;
 
-var ale: integer;
+var limInf, limSup, cantidad: integer;
+
+
+function aleatorioEnRango(limBajo:integer; limAlto:integer): integer;
+  var ale:integer;
+  begin
+	ale:= random(limAlto);
+	if (ale < limBajo) then 
+	  aleatorioEnRango:= (ale DIV 2) + limBajo
+	else
+	  aleatorioEnRango:= ale;
+  end;
+  
+  
+procedure imprimirAleatorios(limInferior:integer; limSuperior:integer; cuantos:integer);
+  var i:integer;
+  begin
+    for i:=0 to cuantos do begin
+      writeln(aleatorioEnRango(limInferior,limSuperior));
+    end;
+  end;
+
 
 begin
      randomize; {Elige una semilla distinta cada vez que se ejecuta el programa.}
@@ -8,10 +29,15 @@ begin
                 {Sin la llamada al procedimiento randomize, en todas las ejecuciones
                  del programa se elige siempre la misma serie de números aleatorios.}
      
-     ale := random (100); {devuelve un valor aleatorio en el intervalo 0 a 99}
-
-     writeln ('El numero aleatorio generado es: ', ale);
-
-	 writeln ('Presione cualquier tecla para finalizar');
-     readln;
+     write('Ingrese el limite inferior: ');
+     readln(limInf);
+     
+     write('Ingrese el limite superior: ');
+     readln(limSup);
+     
+     write('Ingrese la cantidad de numeros a imprimir: ');
+     readln(cantidad);
+     
+     imprimirAleatorios(limInf, limSup, cantidad);
+	
 end.
